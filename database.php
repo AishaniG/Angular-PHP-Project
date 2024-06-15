0ac43e947b9b623a5597ce905a03b9eb9af03d7c
AishaniG <86511579+AishaniG@users.noreply.github.com>
2024-06-15 21:35:55 +0530

    Update database.php

--- database.php
+++ database.php
@@ -3,9 +3,9 @@
 class Database{
     public function getConn(){
         $db_host = "localhost";
-        $db_name = "CTS";
-        $db_user = "CTS_www";
-        $db_pass = "Musicislife@2";
+        $db_name = "";
+        $db_user = "";
+        $db_pass = "";
 
     $dsn = 'mysql:_host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
     try{
